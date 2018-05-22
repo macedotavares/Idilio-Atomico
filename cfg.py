58b09cf8341b3ca6b83c5be1c5661b31035a04ea
Miguel <macedotavares@gmail.com>
2018-05-23 00:11:19 +0100

    working - kind of

--- cfg.py
+++ cfg.py
@@ -10,19 +10,33 @@ NP = ['DT NN' ,'DT JJ NN']
 
 VP = ['sleeps', 'walks']
 
-S = ['NP VP', 'NP VP NP']
+S = ['NP VP', 'NP VP NP', 'NN VP NN']
 
 taglist = {'NN':NN, 'VP':VP, 'DT':DT, 'JJ':JJ, 'NP':NP, 'S':S}
 
-def parse(tag):
-	construction = ''
-	construction = random.choice(tag)
+def parse(start):
+	construction = random.choice(start).split(' ')
 	print(construction)
-	for symbol in construction.split(' '):
-		if symbol in taglist:
-			parse(taglist[symbol])
-
+	repeat = True
 	
+	while repeat:
+		for i in range(0,len(construction)):
+			
+
+			symbol = construction[i]
+			flat=[]			
+			if symbol in taglist:
+				construction[i] = random.choice(taglist[symbol])
+				for x in construction:
+					for y in x.split(' '):
+						flat.append(y)
+				construction = flat
+				
+				
+			else:
+				repeat=False
+
+	print(construction)
 
 
 parse(S)
