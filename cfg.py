9b83e904f11be5a473b8f7b4f6556d93b89343bd
Miguel <macedotavares@gmail.com>
2018-05-24 11:51:26 +0100

    refine

--- cfg.py
+++ cfg.py
@@ -1,25 +1,27 @@
 import random
 
-taglist = {
+grammar = {
 			'NN':['man', 'dog', 'box'],
 			'VP':['sleeps', 'walks'],
 			'DT':['the', 'a', 'some', 'that'],
-			'JJ':['red', 'green', 'large'],
+			'JJ':['red', 'green', 'large', 'JJ JJ'],
 			'NP':['DT NN' ,'DT JJ NN'],
-			'S':['NP VP NP']
+			'S':['NP VP NP'],
+			'Q':['Does S ?']
 			}
 
+taglist = grammar.keys()
 
 def expand(symbol):
-	expanded = random.choice(taglist[symbol])
+	expanded = random.choice(grammar[symbol])
 	return expanded
 
 def check_for_keys(string):
-	keys_list = taglist.keys()
-	intersect = set(string.split(' ')).intersection(set(keys_list))
+	intersect = set(string.split(' ')).intersection(set(taglist))
 	return intersect
 
 def parse(start_symbols):
+	global symbol_list
 	symbol_list=start_symbols.split(' ')
 	for i in range(0,len(symbol_list)):
 		if symbol_list[i] in taglist:
@@ -27,7 +29,9 @@ def parse(start_symbols):
 	symbol_list=' '.join(symbol_list)
 	if check_for_keys(symbol_list):
 		parse(symbol_list)
-	print(symbol_list)
+	else:
+		print(symbol_list)
 
-parse('S')
+parse('Q')
+
 
