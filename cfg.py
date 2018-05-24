7873b57329329ccfbb966db63893b5eafdda60bc
Miguel <macedotavares@gmail.com>
2018-05-24 11:42:58 +0100

    WORKING!!

--- cfg.py
+++ cfg.py
@@ -3,36 +3,31 @@ import random
 taglist = {
 			'NN':['man', 'dog', 'box'],
 			'VP':['sleeps', 'walks'],
-			'DT':['the', 'a', 'some'],
+			'DT':['the', 'a', 'some', 'that'],
 			'JJ':['red', 'green', 'large'],
 			'NP':['DT NN' ,'DT JJ NN'],
 			'S':['NP VP NP']
 			}
 
+
 def expand(symbol):
 	expanded = random.choice(taglist[symbol])
 	return expanded
 
-def parse(start):
-	start = expand(start)
-	construction = start.split(' ')
-	expanded = []
-	
-	for i in range(0,len(construction)):
-		print('expanded construction '+str(i)+': '+str(expanded))
-		expanded = list(construction)
-		
-		word=construction[i]
-		
-		if word in taglist:
-			expansion = expand(word)
-			expanded[i] = str(expansion)
-			print (word+' '+str(i)+' expanded to '+str(expansion))
-		else:
-			pass
-		for i in range(0,len(expanded)):
-			construction += expanded[i]+' '
+def check_for_keys(string):
+	keys_list = taglist.keys()
+	intersect = set(string.split(' ')).intersection(set(keys_list))
+	return intersect
 
+def parse(start_symbols):
+	symbol_list=start_symbols.split(' ')
+	for i in range(0,len(symbol_list)):
+		if symbol_list[i] in taglist:
+			symbol_list[i]=expand(symbol_list[i])
+	symbol_list=' '.join(symbol_list)
+	if check_for_keys(symbol_list):
+		parse(symbol_list)
+	print(symbol_list)
 
 parse('S')
 
