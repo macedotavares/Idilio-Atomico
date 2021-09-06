0b1f32e3c8243aa18b5300b3cf98926010c00c6c
macedotavares <macedotavares@gmail.com>
2021-09-06 17:58:18 +0100

    profiling

--- profiling.py
+++ profiling.py
@@ -3,6 +3,6 @@ import idilioatomico
 
 pr = cProfile.Profile()
 pr.enable()
-pr.run('idilioatomico.make("F",1000)')
+pr.run('idilioatomico.make("F",10000)')
 pr.disable()
 pr.print_stats(sort='cumtime')
